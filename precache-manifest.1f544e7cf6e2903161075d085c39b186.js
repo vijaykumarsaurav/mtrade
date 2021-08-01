446879877910208c9f94a89083019f678a64cf56
vijaykumarsaurav <vijay3.kumar@airtel.com>
2021-08-01 11:18:35 +0530

    Updates

---
+++ precache-manifest.1f544e7cf6e2903161075d085c39b186.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b6b33933307152fd1062a1ce9493c5",
+    "url": "/LearnNew/index.html"
+  },
+  {
+    "revision": "5f8308ef8f14cfa6799f",
+    "url": "/LearnNew/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "6a4cdd1a58cb4c232eb8",
+    "url": "/LearnNew/static/css/main.690f9285.chunk.css"
+  },
+  {
+    "revision": "5f8308ef8f14cfa6799f",
+    "url": "/LearnNew/static/js/2.88e053e8.chunk.js"
+  },
+  {
+    "revision": "6a4cdd1a58cb4c232eb8",
+    "url": "/LearnNew/static/js/main.caa02667.chunk.js"
+  },
+  {
+    "revision": "27862c46635e0ac6d6c2",
+    "url": "/LearnNew/static/js/runtime~main.1cb0fa5c.js"
+  },
+  {
+    "revision": "92bbdbd4ea257086271b29f4a9e1c43b",
+    "url": "/LearnNew/static/media/mylogo.92bbdbd4.png"
+  }
+]);
