b52636f5bd2ff1d3f2928946926df896ec4c9629
B0208058 <B0208058@LTB0208058-MAC.local>
2021-09-01 03:27:45 +0530

    Updates

---
+++ precache-manifest.0d2f33badee42ed378a817962136a29a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d7b5bf2a45b4f4cb6261aa374fadfe",
+    "url": "/mtrade/index.html"
+  },
+  {
+    "revision": "bb2cf4712daeefd2895a",
+    "url": "/mtrade/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "6db760f2db21496060ff",
+    "url": "/mtrade/static/css/main.846c3ea8.chunk.css"
+  },
+  {
+    "revision": "bb2cf4712daeefd2895a",
+    "url": "/mtrade/static/js/2.56179732.chunk.js"
+  },
+  {
+    "revision": "6db760f2db21496060ff",
+    "url": "/mtrade/static/js/main.02a5d3d3.chunk.js"
+  },
+  {
+    "revision": "e8b8ba76f00acf92c648",
+    "url": "/mtrade/static/js/runtime~main.72b3d60e.js"
+  },
+  {
+    "revision": "92bbdbd4ea257086271b29f4a9e1c43b",
+    "url": "/mtrade/static/media/mylogo.92bbdbd4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mtrade/precache-manifest.bd9e6aa4b6a12500c28238e0b5d465ec.js"
+  "/mtrade/precache-manifest.0d2f33badee42ed378a817962136a29a.js"
 );
 
 self.addEventListener('message', (event) => {
