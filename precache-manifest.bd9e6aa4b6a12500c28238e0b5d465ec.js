845af0de0e64286be0ad66d97eb532eaa0188820
B0208058 <B0208058@LTB0208058-MAC.local>
2021-08-31 14:45:56 +0530

    Updates

---
+++ precache-manifest.bd9e6aa4b6a12500c28238e0b5d465ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9a59db483fd50f513e6c4c15f11e97",
+    "url": "/mtrade/index.html"
+  },
+  {
+    "revision": "d91e7b30174456060b2c",
+    "url": "/mtrade/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "28dc170f5424941a64ce",
+    "url": "/mtrade/static/css/main.846c3ea8.chunk.css"
+  },
+  {
+    "revision": "d91e7b30174456060b2c",
+    "url": "/mtrade/static/js/2.77d4b5ac.chunk.js"
+  },
+  {
+    "revision": "28dc170f5424941a64ce",
+    "url": "/mtrade/static/js/main.70ab2a5c.chunk.js"
+  },
+  {
+    "revision": "e8b8ba76f00acf92c648",
+    "url": "/mtrade/static/js/runtime~main.72b3d60e.js"
+  },
+  {
+    "revision": "92bbdbd4ea257086271b29f4a9e1c43b",
+    "url": "/mtrade/static/media/mylogo.92bbdbd4.png"
+  }
+]);
