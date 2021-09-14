daa24af424ec72da7147dbefe0a6456993fc492f
vijaykumarsaurav <vijaykumarsaurav@gmail.com>
2021-09-15 00:17:09 +0530

    Updates

---
+++ precache-manifest.3ad7806fb2ac25619f420d6480280b78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110d59703b27a183b24b85936fa29a63",
+    "url": "/mtrade/index.html"
+  },
+  {
+    "revision": "07c2c4b08ed275d5d6fc",
+    "url": "/mtrade/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "7f49da798990c69bdf82",
+    "url": "/mtrade/static/css/main.846c3ea8.chunk.css"
+  },
+  {
+    "revision": "07c2c4b08ed275d5d6fc",
+    "url": "/mtrade/static/js/2.7298e40d.chunk.js"
+  },
+  {
+    "revision": "7f49da798990c69bdf82",
+    "url": "/mtrade/static/js/main.d7240f42.chunk.js"
+  },
+  {
+    "revision": "e8b8ba76f00acf92c648",
+    "url": "/mtrade/static/js/runtime~main.72b3d60e.js"
+  },
+  {
+    "revision": "92bbdbd4ea257086271b29f4a9e1c43b",
+    "url": "/mtrade/static/media/mylogo.92bbdbd4.png"
+  }
+]);
